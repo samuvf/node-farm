81e02ac366fbcc3703e689d49c80392ab36b2fb7
samuvf <samuelvf9@hotmail.com>
2025-10-08 11:46:50 -0300

    feat: overview route

--- index.js
+++ index.js
@@ -3,14 +3,51 @@ const http = require("http");
 const url = require("url");
 const port = 8000;
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%PLACE%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   path = req.url;
-  if ((path == "/") | (path == "/overview")) {
-    res.end("afojgjghsaalgj");
-  } else if (path == "/products") {
+  if ((path === "/") | (path === "/overview")) {
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const overview = tempOverview.replace("{%PRODUCT_CARD%}", cardHtml);
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(overview);
+  } else if (path === "/products") {
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
-    res.end("Page not Found");
+    res.end("<h1>Page not Found</h1>");
   }
 });
 
